71788d7525aaefc3dcd4e8c63d63b0fe81d6d7e4
Eric Owen <ericowen@erics-mbp.lan>
2021-08-09 13:55:07 -0400

    initial

---
+++ src/models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('age must be a pos num');
+      }
+    },
+    default: 0,
+  },
+
+  password: {
+    required: true,
+    trime: true,
+    type: String,
+    validate(value) {
+      if (
+        !validator.isStrongPassword(value, {
+          minLength: 8,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+        })
+      ) {
+        throw new Error('Is Not Strong Password');
+      }
+    },
+  },
+
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('email not valid');
+      }
+    },
+    trim: true,
+    lowercase: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  
+  next();
+});
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
