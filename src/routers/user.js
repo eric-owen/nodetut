8002422d8ee1c66235d67e2bb2443732d830f469
Eric Owen <ericowen@erics-mbp.lan>
2021-08-16 12:15:01 -0400

    logout all

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
@@ -23,6 +24,30 @@ router.post('/users/login', async (req, res) => {
   } catch (err) {
     res.status(400).send();
   }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+    await req.user.save();
+
+    res.send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/logoutall', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users', async (req, res) => {
